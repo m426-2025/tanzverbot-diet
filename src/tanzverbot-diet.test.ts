de20335547551e95a73e417fec3f6554c329d828
Kush <kushsrigiri@gmail.com>
2025-05-16 15:04:14 +0200

    [Aufgabe 2] Tests eingefügt, gelöst | Files: src/tanzverbot-diet.test.ts

--- src/tanzverbot-diet.test.ts
+++ src/tanzverbot-diet.test.ts
@@ -1,5 +1,13 @@
 import { calcDateOnDiet, Sex } from "./tanzverbot-diet";
 
 test("Tanzverbot Diet", () => {
-  expect(calcDateOnDiet(74, 100, 1.86, 38, Sex.Male)).toBeGreaterThan(0);
+  expect(calcDateOnDiet(70, 75, 1.80, 30, Sex.Male)).toBeGreaterThan(0);
+});
+
+test("Tanzverbot Diet", () => {
+  expect(calcDateOnDiet(60, 65, 1.65, 28, Sex.Female)).toBeGreaterThan(0);
+});
+
+test("Tanzverbot Diet", () => {
+  expect(calcDateOnDiet(80, 80, 1.75, 40, Sex.Male)).toBe(0);
 });
